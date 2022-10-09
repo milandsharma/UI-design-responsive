7e2ec599ae43e5b6afc6f950547a5e7b7af91080
miland sharma <milandsharma24@gmail.com>
2022-10-09 17:18:06 +0530

    responsive correction

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //jshint esversion: 6
 let sidebarlogo = document.querySelector(".sidebarlogo");
 let sideBar = document.querySelector(".sidebar");
+let page = document.querySelector(".pages h3");
 
 let count = 0;
 
-if(window.innerWidth < 500){
+if(window.innerWidth <= 600){
+    
     sidebarlogo.addEventListener("click",function(){
         if(count == 0){
             sideBar.style.visibility = "visible";
@@ -19,17 +21,24 @@ if(window.innerWidth < 500){
     }
     );
 }else{
+    page.style.display = "none";
     sidebarlogo.addEventListener("click",function(){
         if(count == 0){
-            sideBar.style.width = "20%";
+            sideBar.style.width = "16vw";
+            page.style.display = "block";
             setTimeout(function(){
-                sideBar.style.width = "5%";
+                sideBar.style.width = "5vw";
             },5000);
             count = 1;
         }else{
-            sideBar.style.width = "5%";
+            page.style.display = "none";
+            sideBar.style.width = "5vw";
             count = 0;
         }
     });
 }
-
+if(sideBar.style.width <= "8vw" || sideBar.style.width == "5%"){
+    page.style.display = "none";
+}else{
+    page.style.display = "block";
+}
